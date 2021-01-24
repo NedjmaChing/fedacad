dd5746ec889c70107c1731dec7a742d1f64af7e8
barman47 <nomsouzoanya@yahoo.co.uk>
2021-01-24 09:06:19 +0100

    Posted Admission List

--- src/components/home/News.js
+++ src/components/home/News.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import slide4 from '../../assets/img/slide4.jpg';
 import {Link} from 'react-router-dom';
+import TextClamp from 'react-string-clamp';
 
 import application from '../../assets/img/application.jpg';
+import admissionList from '../../assets/img/slide2.jpg';
 
 
 const News = () => (
@@ -12,10 +14,33 @@ const News = () => (
         <div className="news-container">
             <article>
                 <div>
+                    <img src={admissionList} alt="Federal Government Academy Suleja, Entrance Examination" />
+                </div>
+                <div>
+                    <a href="https://firebasestorage.googleapis.com/v0/b/fedacad-927ea.appspot.com/o/FGA%20Admission%20List.pdf?alt=media&token=89ddd05c-1285-491c-aa1a-a1a3a3e3d6b8" target="_blank" rel="noopener noreferrer">
+                        <TextClamp 
+                            text="2020/2021 Admission List"
+                            lines={2}
+                            className="Application for 2020/2021 Academic Session now Open"
+                        />
+                    </a>
+                    <p><span className="mdi mdi-calendar"></span>&nbsp;&nbsp;&nbsp;24th January, 2021</p>
+                    <p>All successful candidates are to collect their Letters of Admission from the Principal's Office beginning from Monday 25th January 2021.</p>
+                    <Link to="/news#admission-list">Read More</Link>
+                </div>
+            </article>
+            <article>
+                <div>
                     <img src={application} alt="Federal Government Academy Suleja, Entrance Examination" />
                 </div>
                 <div>
-                    <a href="https://firebasestorage.googleapis.com/v0/b/fedacad-927ea.appspot.com/o/admission%20form.pdf?alt=media&token=a64f246a-6ef6-4b50-9f1e-d3e40d91c1d2" target="_blank" rel="noopener noreferrer"><h5>Application for 2020/2021 Academic Session now Open</h5></a>
+                    <a href="https://firebasestorage.googleapis.com/v0/b/fedacad-927ea.appspot.com/o/admission%20form.pdf?alt=media&token=a64f246a-6ef6-4b50-9f1e-d3e40d91c1d2" target="_blank" rel="noopener noreferrer">
+                        <TextClamp 
+                            text="Application for 2020/2021 Academic Session now Open"
+                            lines={2}
+                            className="title"
+                        />
+                    </a>
                     <p><span className="mdi mdi-calendar"></span>&nbsp;&nbsp;&nbsp;19th August,2020</p>
                     <p>The school for the gifted and talented is inviting students to apply for the 2020/2021 academic session starting in September. Download the form by clicking on the link above.</p>
                 </div>
@@ -25,7 +50,13 @@ const News = () => (
                     <img src={slide4} alt="Football game" />
                 </div>
                 <div>
-                    <Link to="/news"><h5>Federal Ministry Of Education, Federal Government Academy, Suleja 2020 Entrance Examination</h5></Link>
+                    <Link to="/news">
+                        <TextClamp 
+                            text="Federal Ministry Of Education, Federal Government Academy, Suleja 2020 Entrance Examination"
+                            lines={2} 
+                            className="title"
+                        />
+                    </Link>
                     <p><span className="mdi mdi-calendar"></span>&nbsp;&nbsp;&nbsp;23rd August,2020</p>
                     <p>The Federal Government Academy Suleja is a National Centre for nurturing gifted and talented children of Secondary School age. Students of the Academy are on Federal Government Scholarship which covers their tuition, boarding, feeding, uniforms and basic text and exercise books only.</p>
                 </div>
--- src/components/news/News.js
+++ src/components/news/News.js
@@ -11,28 +11,45 @@ const News = () => (
             linkText="News" 
             title="Latest News" 
         />
-        <section className="about-fedacad">
-            <h4>
-                FEDERAL MINISTRY OF EDUCATION, FEDERAL GOVERNMENT ACADEMY,<br />  SULEJA 2020 ENTRANCE EXAMINATION
-            </h4>
-            <p>
-                The Federal Government Academy Suleja is a National Centre for nurturing gifted and talented children of Secondary School age. Students of the Academy are on Federal Government Scholarship which covers their tuition, boarding, feeding, uniforms and basic text and exercise books only.
-            </p>
-            <p>
-                The Gifted Education Programme gives opportunity to brilliant, gifted and talented, especially economically disadvantaged students who lack financial resources for this kind of education. Every state of the Federation has this class of students.
-            </p>
-            <p>
-                It is therefore the wish of the Hon. Minister of Education Malam Adamu Adamu, that States Ministries of Education should use this opportunity to sponsor their representatives to the Centre to fill their slots at a fee of N1,500.00 only per candidate. The Entrance Examination will take place on 26th September, 2020.
-            </p>
-            <p>
-                This great opportunity should be extended to both Urban and Rural dwellers irrespective of sex. Please download the form and other necessary details from www.education.gov.ng .
-            </p>
-            <p>
-                Dr. Asma’u Y. Abdullahi
-            </p>
-            <p>
-                Director/Principal, FGA
-            </p>
+        <section className="news">
+            <article id="admission-list">
+                <h5>
+                    2020/2021 Admission List
+                </h5>
+                <p>
+                    All successful candidates are to collect their Letters of Admission from the Principal's Office beginning from Monday 25th January 2021.
+                </p>
+                <p>
+                    The proof of ownership is the photo card issued to the candidates during the Entrance Examination.
+                </p>
+                <p>Resumption is on Saturday, 30th January, 2021.</p>
+                <p>Time: 9:00 am to 5:00 p.m</p>
+                <p>Congratulations!</p>
+                <a href="https://firebasestorage.googleapis.com/v0/b/fedacad-927ea.appspot.com/o/FGA%20Admission%20List.pdf?alt=media&token=89ddd05c-1285-491c-aa1a-a1a3a3e3d6b8" target="_blank" rel="noopener noreferrer">View 2020/2021 Admission List</a>
+            </article>
+            <article>
+                <h5>
+                    FEDERAL MINISTRY OF EDUCATION, FEDERAL GOVERNMENT ACADEMY,<br />  SULEJA 2020 ENTRANCE EXAMINATION
+                </h5>
+                <p>
+                    The Federal Government Academy Suleja is a National Centre for nurturing gifted and talented children of Secondary School age. Students of the Academy are on Federal Government Scholarship which covers their tuition, boarding, feeding, uniforms and basic text and exercise books only.
+                </p>
+                <p>
+                    The Gifted Education Programme gives opportunity to brilliant, gifted and talented, especially economically disadvantaged students who lack financial resources for this kind of education. Every state of the Federation has this class of students.
+                </p>
+                <p>
+                    It is therefore the wish of the Hon. Minister of Education Malam Adamu Adamu, that States Ministries of Education should use this opportunity to sponsor their representatives to the Centre to fill their slots at a fee of N1,500.00 only per candidate. The Entrance Examination will take place on 26th September, 2020.
+                </p>
+                <p>
+                    This great opportunity should be extended to both Urban and Rural dwellers irrespective of sex. Please download the form and other necessary details from www.education.gov.ng .
+                </p>
+                <p>
+                    Dr. Asma’u Y. Abdullahi
+                </p>
+                <p>
+                    Director/Principal, FGA
+                </p>
+            </article>
         </section>
   </>
 );
